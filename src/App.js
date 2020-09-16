527040def34f5faca5bbf7057a0502f3f1e45f32
petersoleeh <petersoleeh@gmail.com>
2020-09-16 12:21:46 +0300

    Static data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core'
+
+import SearchBar from './components/SearchBar'
+
+import SearchResults from './components/SearchResults'
+
+import gbif from './api/gbif'
+
+
 class App extends React.Component {
+    state = {
+        bees: [],
+        selectedBees: null
+    }
+    handleSubmit = async (searchTerm) => {
+        const response = await gbif.get('search', {
+            params: {
+                q: searchTerm, 
+                country: 'KE'
+            }
+        })
+
+        this.setState({bees: response.data.results, selectedBees: response.data.results[0]})
+    }
+
     render() {
+        const {selectedBees} =this.state
+
         return(
-            <h1>ICIPE APP</h1>
+            <Grid container justify='center' spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar  onFormSubmit = {this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <SearchResults  bees={selectedBees}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* CARD DISPLAY */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import {Paper, CardActionArea,CardContent,CardMedia, Button, Typography, Card, Container, Grid, CardActions} from '@material-ui/core'
+
+const SearchResults = ({bees}) => {
+
+    const cards = [1, 2, 3]
+
+    return (
+        
+        <React.Fragment>
+            <Container maxWidth='md'>
+                <Grid container spacing={4}>
+                    {cards.map((card) =>
+                        <Grid item key = {card} xs={12} sm={6} md={4}>            
+                            <Card>
+                                <CardActionArea>
+                                <CardMedia
+                                    component = 'img'
+                                    alt= 'bees'
+                                    height='140'
+                                    image='https://source.unsplash.com/dyGWrmJ_i4E'
+                                    title="bees"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant='h5' component='h2'>
+                                        APIS
+                                    </Typography>
+                                    <Typography variant='body2' color='textSecondary' component='p'>
+                                        Bees are a widespread group of squamate insects, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color='primary'>
+                                        Learn More
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid> 
+                    )}
+                </Grid>
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export  default SearchResults

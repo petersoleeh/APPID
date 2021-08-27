1e6dc547ff27546cb9214b800cda0b362f168448
petersoleeh <petersoleeh@gmail.com>
2021-08-27 10:37:13 +0300

    Add basemap layers

--- src/App.js
+++ src/App.js
@@ -2,181 +2,53 @@ import React from "react";
 
 import "./App.css";
 
-import Home from './components/pages/Home'
+import Home from "./components/pages/Home";
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-
-// import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import NavBar from "./components/NavBar";
-import Footer from './components/pages/Footer'
+import Footer from "./components/pages/Footer";
 import About from "./components/pages/About";
-import ErrorFourOFour from "./components/pages/ErrorFourOFour"
+import ErrorFourOFour from "./components/pages/ErrorFourOFour";
 
-import SearchBar from "./components/SearchBar";
 import FullMap from "./components/FullMap";
 
-import ObservationDetail from './components/ObservationDetail'
-import ObservationList from './components/ObservationList'
+import ObservationDetail from "./components/ObservationDetail";
+import ObservationList from "./components/ObservationList";
 import SearchResults from "./components/SearchResults";
 
-import Application from "./components/Application";
-import UserProvider from "./providers/UserProvider";
-
-import SignIn from "./components/SignIn"
-
-// import SearchResults from "./components/SearchResults";
-
-// import ObservationList from "./components/ObservationList";
-
-// import GbifResults from "./components/GbifResults";
-
-// import MapGbifData from "./components/MapData";
-
-import gbif from "./api/gbif";
-// import gbif2 from "./api/gbif2";
-
 class App extends React.Component {
-  
-  
-  constructor() {
-    window.scrollTo(0, 0);
-    super();
-
-    this.state = {
-      bees: [],
-      selectedBees: null,
-      // gbifData: [],
-      loading: false,
-      // MapData: []
-    };
-  }
-  
-
-  // async componentDidMount(){
-    // handleSubmit()
-    // .then((res)=> {
-    //   this.setState({
-    //     bees:res.data
-    //   })
-    // })
-  // }
-
-  handleSubmit = async (searchTerm) => {
-    const response = await gbif.get("search", {
-      params: {
-        q: searchTerm,
-        limit: 10
-      },
-    });
-    console.log(response.data);
-
-    this.setState({
-      bees: response.data.results,
-      selectedBees: response.data.results[0],
-    });
-  };
-
-  // async componentDidMount() {
-  //   await gbif2.get("search", {
-  //     params: {
-  //       limit: 6
-  //     }
-  //   }).then((res) => {
-  //     this.setState({
-  //       gbifData: res.data,
-  //       MapData: res.data,
-  //       loading: true,
-  //     });
-  //   });
-  // }
-
-
-
-
-
-
   render() {
-    const { selectedBees, bees } = this.state;
-    // const { gbifData, loading } = this.state;
-    // const { MapData } = this.state;
-
     return (
-      // <React.Fragment>
-      //   <Router>
-      //     <NavBar />
-      //     <Switch>
-      //       <Route path="/" exact />
-      //     </Switch>
-      //     <Grid container justify="center" spacing={10}>
-      //       <Grid item xs={12}>
-      //         <Grid container spacing={10}>
-      //           <Grid item xs={12}>
-      //             <SearchBar onFormSubmit={this.handleSubmit} />
-      //           </Grid>
-      //           <Grid item xs={12}>
-                  // <SearchResults bees={selectedBees}   />
-      //             <GbifResults gbifData={gbifData} loading={loading} />
-      //           </Grid>
-      //           <Grid item xs={12}>
-      //             <ObservationList bees={bees} />
-      //           </Grid>
-      //           {/* <Grid item xs={6}>
-      //             Display footer
-      //           </Grid> */}
-      //         </Grid>
-      //       </Grid>
-      //     </Grid>
-      //   </Router>
-      // </React.Fragment>
       <React.Fragment>
-
-    {/* <UserProvider>
-      <Application />
-    </UserProvider> */}
-    
         <Router>
           <NavBar />
-      
-          {/* <Footer /> */}
+
           <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact component={Home} />
             <Route path="/observations" exact component={ObservationList} />
             <Route path="/about" exact component={About} />
-            <Route path='/observations/:id' exact component={ObservationDetail} />
-            <Route path='/maps' exact component={FullMap} />
-            <Route path='/results' exact component={SearchResults} bees={bees} />
-
+            <Route
+              path="/observations/:id"
+              exact
+              component={ObservationDetail}
+            />
+            <Route path="/maps" exact component={FullMap} />
+            <Route
+              path="/results"
+              exact
+              component={SearchResults}
+            />
 
             <Route path="*" component={ErrorFourOFour} />
           </Switch>
-          {/* <Container style={{ marginTop: "30px" }}>
-            <Paper style={{ backgroundColor: "#cfe8fc", height: "60vh" }}>
-              here
-            </Paper>
-            <Grid item xs={12} style={{ marginTop: "50px"}}>
-              <Typography variant="h4">Recent Observations</Typography>
-            </Grid>
-            <Grid item xs={12}  style={{ marginTop: "50px" }}>
-              <GbifResults gbifData={gbifData} loading={loading} />
-            </Grid>
-            <Grid item xs={12} style={{ marginTop: "50px"}}>
-              <Typography variant="h4">Map </Typography>
-            </Grid>
-            <Grid item xs={12} style={{ marginTop: "50px"}}>
-              <MapGbifData MapData={MapData} loading={loading}/>
-            </Grid>
-            <Grid item xs={12} style={{ marginTop: "50px"}}>
-              <Typography variant="h4">Partners </Typography>
-            </Grid>
-            <Grid item xs={12} style={{ marginTop: "50px"}}> */}
-              {/* <MapGbifData MapData={MapData} loading={loading}/> */}
-            {/* </Grid>
-          </Container> */}
           <Footer />
-
         </Router>
-        
       </React.Fragment>
     );
   }
--- src/components/FullMap.js
+++ src/components/FullMap.js
@@ -5,19 +5,25 @@ import Skeleton from "@material-ui/lab/Skeleton";
 import Box from "@material-ui/core/Box";
 import { Link } from "react-router-dom";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "react-leaflet-markercluster/dist/styles.min.css";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let DefaultIcon = L.icon({
-    iconUrl: "/images/bee_drop.png",
-    iconSize: [25,41],
-    iconAnchor: [12,41],
-    shadowUrl: iconShadow
+  iconUrl: "/images/bee_drop.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -69,31 +75,47 @@ function FullMap() {
           maxZoom={18}
           style={{ width: "100%", height: "680px" }}
         >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <MarkerClusterGroup>
-            {MapData.map((data) => (
-              <Marker
-                key={data._id}
-                position={[data._geolocation[0], data._geolocation[1]]}
-              >
-                <Popup>
-                  <Link to={`/observations/${data._id}`}>
-                    <img
-                      src={data._attachments
-                        .slice(0, 1)
-                        .map((img) => img.download_url)}
-                      width="150"
-                      height="150"
-                      alt={data.genericName}
-                    />
-                  </Link>
-                </Popup>
-              </Marker>
-            ))}
-          </MarkerClusterGroup>
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name="OpenStreetMap">
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="ESRI world">
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png"
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="OpenToPoMap">
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+              />
+            </LayersControl.BaseLayer>
+            <MarkerClusterGroup>
+              {MapData.map((data) => (
+                <Marker
+                  key={data._id}
+                  position={[data._geolocation[0], data._geolocation[1]]}
+                >
+                  <Popup>
+                    <Link to={`/observations/${data._id}`}>
+                      <img
+                        src={data._attachments
+                          .slice(0, 1)
+                          .map((img) => img.download_url)}
+                        width="150"
+                        height="150"
+                        alt={data.genericName}
+                      />
+                    </Link>
+                  </Popup>
+                </Marker>
+              ))}
+            </MarkerClusterGroup>
+          </LayersControl>
         </MapContainer>
       </React.Fragment>
     );
